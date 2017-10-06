6c4fe568a354125a44c30fca20d89bd97ec2adac
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2017-10-06 20:16:14 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string string;
+	int result, x;
+	char op;
+	getline(cin,string);
+	istringstream stream(string);
+	stream>>result;
+        bool stop = false; 
+	while(stream>>op) {
+		stream>>x;
+		if(op=='+') result = result+x;
+	        else if(op=='-') result = itog-x;
+	        else if(op=='*') result = itog*x;
+	        else if(op=='/') {
+			if (middle == 0) cout<<"Error";
+			else result = result/x;
+			}
+                else {cout<<"Error"; stop = true;}
+	}
+        if (!stop) cout<<result;
+}
